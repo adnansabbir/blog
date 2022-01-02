e92e902b8746e79ce936411ea97b0f2f8ab4e39c
adnan-sabbir <adnan.sabbir@g.bracu.ac.bd>
2022-01-03 04:18:53 +0600

    Updated comment list ui and added scaffold to easily run deployments

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -3,6 +3,7 @@ import React from "react";
 const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => {
     let content;
+    let style;
 
     if (comment.status === "approved") {
       content = comment.content;
@@ -10,13 +11,15 @@ const CommentList = ({ comments }) => {
 
     if (comment.status === "pending") {
       content = "This comment is awaiting moderation";
+      style = {color: 'orange'};
     }
 
     if (comment.status === "rejected") {
       content = "This comment has been rejected";
+      style = {color: 'red'};
     }
 
-    return <li key={comment.id}>{content}</li>;
+    return <li key={comment.id} style={style}>{comment.content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
